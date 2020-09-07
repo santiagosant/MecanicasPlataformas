9b33bd1bde15b47ca96aaa0f6bf222a93303c2ea
santiagosant <santidevargas@gmail.com>
2020-09-07 10:57:45 -0300

    add player controller, maxSpeed

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 2f;
+    public float maxSpeed = 5f;
+    private Rigidbody2D rb2d;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        rb2d.AddForce(Vector2.right * speed * h);
+
+        if(rb2d.velocity.x > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+        }
+
+        if (rb2d.velocity.x < -maxSpeed)
+        {
+            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+        }
+    }
+}
