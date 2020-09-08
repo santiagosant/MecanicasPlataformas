f6daa2999b729269a5b1ad19ba0d55ac73833b1d
santiagosant <santidevargas@gmail.com>
2020-09-07 21:05:46 -0300

    Puede saltar/ falta perfeccionarlo

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 2f;
     public float maxSpeed = 5f;
     private Rigidbody2D rb2d;
+    public float jumpPower = 6.5f;
+    private bool jump;
 
 
     // Start is called before the first frame update
@@ -16,9 +18,18 @@ public class PlayerController : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            jump = true;
+        }
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
+        //movimiento horizontal y control de velocidad
         float h = Input.GetAxis("Horizontal");
 
         rb2d.AddForce(Vector2.right * speed * h);
@@ -31,6 +42,14 @@ public class PlayerController : MonoBehaviour
         if (rb2d.velocity.x < -maxSpeed)
         {
             rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+        }
+
+        //Salto
+
+        if (jump)
+        {
+            rb2d.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            jump = false;
         }
     }
 }
