bf12afb26344626d14d58a60f7995caae59e8572
santiagosant <santidevargas@gmail.com>
2020-09-13 17:12:22 -0300

    Pantalla principal

---
+++ Assets/Script/ControllerScena.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControllerScena : MonoBehaviour
+{
+    public void CambiarEscena(string nombre)
+    {
+        print("Cambiar escena a " + nombre);
+        SceneManager.LoadScene(nombre);
+    }
+
+    public void Salir()
+    {
+        print("saliendo");
+        Application.Quit();
+    }
+}
